f3a74140309b816ad74cb6a567687c8ad335d8d8
Anders Valind <anders.valind@gmail.com>
2021-04-07 13:08:24 +0200

    Ripped out the vector implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cstdint>
 #include "tga.h"
 #include "draw.h"
 #include "vector.h"
+
+
+template <typename T, unsigned int N>
+bool is_perpendicular(hank::Vector<T, N> &a, hank::Vector<T, N> &b) {
+    return a.dot(b) < __FLT_EPSILON__;
+}
 
 
 int main(int argc, char **argv) {
@@ -10,12 +17,21 @@ int main(int argc, char **argv) {
     //hank::Vector3<float> position = 2.0 * origo;
 
 
+    hank::Vector<int, 2> i2vec({1,2});
+    std::cout << i2vec << std::endl;
     
-    hank::TGAImage im(400, 400);
+    hank::Vector<float, 3> f4u({1.0, 0.0, 0.0});
+    hank::Vector<float, 3> f4v({0.0,1.0,0.0});
+    
+    if(is_perpendicular(f4u, f4v)) {
+        std::cout << "Now it's time for lunch!" << std::endl;
+    }
+
+    /*hank::TGAImage im(400, 400);
     im.fill(hank::rgb(0,0,0));
     hank::line(im, 100,100,300,300, hank::rgb(255,0,0));
     hank::line(im, 50, 75, 363, 213, hank::rgb(0,255,0));
-    im.to_file("test.tga");
+    im.to_file("test.tga");*/
     
 
     //std::cout << position << std::endl;
--- vector.h
+++ vector.h
@@ -3,77 +3,114 @@
 
 #include <iostream>
 #include <cmath>
+#include <initializer_list>
 
 namespace hank {
 
-template <typename T>
-class Vector3 {
+template <typename T, unsigned int N>
+class Vector {
+  private:
+    T elem[N];
   public:
-    T x;
-    T y;
-    T z;
-
-    Vector3() {};
-    Vector3(T _x, T _y, T _z): 
-      x(_x), y(_y), z(_z) {};
-    Vector3(const Vector3<T>& rhs) {
-      this->x = rhs.x;
-      this->y = rhs.y;
-      this->z = rhs.z;
-    };
-
-    T length() {
-      return std::sqrt(
-        this->x * this->x + 
-        this->y * this->y + 
-        this->z * this->z);
-    }
-
-    Vector3<T> norm() {
-      T len = this->length();
-      return Vector3<T>(this->x / len,
-                        this->y / len,
-                        this->z / len);
-    }
-
-    Vector3<T> operator/(T a) {
-      return Vector3<T>(this->x / a,
-                        this->y / a,
-                        this->z / a);
-    }
-
-    Vector3<T> operator+(const Vector3<T>& a) {
-      return Vector3<T>(this->x + a.x,
-                        this->y + a.y,
-                        this->z + a.z);   
-    };
-
-    Vector3<T> operator*(T a) {
-      return Vector3<T>(this->x * a,
-                        this->y * a,
-                        this->z * a);
-    };
     
-    Vector3<T> operator-(const Vector3<T>& a) {
-      return Vector3<T>(this->x - a.x,
-                        this->y - a.y,
-                        this->z - a.z);
+    Vector(std::initializer_list<T> l) {
+      int idx = 0;
+      for(auto it = std::begin(l); it != std::end(l); ++it) {
+        if(idx >= N)
+          break;
+        this->elem[idx] = *it;
+        idx++;
+      }
+      // handle the case where we only initialize part of the vector,
+      // for now, the rest of the elements are just zeroes.
+      while(idx < N) {
+        this->elem[idx] = 0;
+        idx++;
+      }
     };
 
-    friend std::ostream& operator<<(std::ostream& os, const Vector3<T>& v) {
-      os << "vector(" << v.x << ", " << v.y << ", " << v.z << ")";
-      return os;
+    Vector() {};
+
+    Vector(const Vector<T, N>& rhs) {
+      for(int c = 0; c < N; c++) {
+        this->elem[c] = rhs.elem[c];
+      }
     }
 
-    friend Vector3<T> operator*(T a, const Vector3<T>& b) {
-      return Vector3<T>(
-        a * b.x,
-        a * b.y,
-        a * b.z
-      );
+    T& operator[] (int index) {
+      return this->elem[index];
+    }
+
+    Vector<T, N> operator+(const Vector<T, N>& a) {
+      Vector<T, N> ret;
+      for(int c = 0; c < N; c++)
+        ret.elem[c] = this->elem[c] + a.elem[c];
+      return ret;
     };
-};
+
+    Vector<T, N> operator-(const Vector<T, N>& a) {
+      Vector<T, N> ret;
+      for(int c = 0; c < N; c++)
+        ret.elem[c] = this->elem[c] - a.elem[c];
+      return ret;
+    };
+
+    Vector<T, N> operator*(T a) {
+      Vector<T, N> ret;
+      for(int c = 0; c < N; c++)
+        ret.elem[c] = this->elem[c] * a;
+      return ret;
+    };
+
+    Vector<T, N> operator/(T a) {
+      Vector<T, N> ret;
+      for(int c = 0; c < N; c++)
+        ret.elem[c] = this->elem[c] / a;
+      return ret;
+    };
+
+    T l2_norm() {
+      T l2_squared;
+      for(int c = 0; c < N; c++)
+        l2_squared += this->elem[c] * this->elem[c];
+      return std::sqrt(l2_squared);
+    };
+
+    Vector<T, N> normalize() {
+      T sf = this->l2_norm();
+      Vector<T, N> ret;
+      for(int c = 0; c < N; c++)
+        ret.elem[c] = this->elem[c] / sf;
+      return ret;
+    };
+
+    T dot(const Vector<T, N>& other) {
+      T ret;
+      for(int c = 0; c < ret; c++)
+        ret += this->elem[c] * other.elem[c];
+      return ret;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector<T, N>& v) {
+      os << "vector(" << v.elem[0];
+      for(int c = 1; c < N; c++) {
+        os << ", " << v.elem[c];
+      }
+      os << ")";
+      return os;
+    };
+
+    friend Vector<T, N> operator*(T a, const Vector<T, N>& b) {
+      Vector<T, N> ret;
+      for(int c = 0; c < N; c++)
+        ret.elem[c] = a * b.elem[c];
+      return ret;
+    };
 
 };
+
+
+};
+
 
 #endif /* VECTOR_H_ */
